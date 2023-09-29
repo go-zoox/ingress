3b950f6637aaa7d31656faffd1c769277e6c000e
Zero <tobewhatwewant@outlook.com>
2023-09-29 12:27:27 +0800

    fix: dns use net instead of powerful dns client

--- core/dns_test.go
+++ core/dns_test.go
@@ -4,20 +4,14 @@ import "testing"
 
 func TestCheckDNS(t *testing.T) {
 	s1 := &core{}
-	ok, _, err := s1.CheckDNS("notfound.domain")
+	_, err := s1.CheckDNS("notfound.domain")
 	if err == nil {
 		t.Fatal("should be error")
 	}
-	if ok {
-		t.Fatal("should be false")
-	}
 
 	s2 := &core{}
-	ok, _, err = s2.CheckDNS("baidu.com")
+	_, err = s2.CheckDNS("baidu.com")
 	if err != nil {
 		t.Fatal(err)
-	}
-	if !ok {
-		t.Fatal("should be true")
 	}
 }
